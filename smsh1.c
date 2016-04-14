4a765b8426eb0a40d2b91ed0038ed094c2bb2fab
stopsecret <sam-i-am@maine.rr.com>
2016-04-14 13:52:55 -0400

    Updated to include "cd .."

--- smsh1.c
+++ smsh1.c
@@ -1,7 +1,7 @@
 /**  smsh1.c  small-shell version 1
- **		first really useful version after prompting shell
- **		this one parses the command line into strings
- **		uses fork, exec, wait, and ignores signals
+ **             first really useful version after prompting shell
+ **             this one parses the command line into strings
+ **             uses fork, exec, wait, and ignores signals
  **/
 
 #include <stdio.h>
@@ -11,14 +11,14 @@
 #include <signal.h>
 #include "smsh.h"
 
-#define	DFL_PROMPT	"$$$"
+#define DFL_PROMPT      "$$$"
 
 int main()
 {
   int nRetCode;
-  char	*cmdline, *prompt, **arglist;
-  int	result;
-  void	setup();
+  char  *cmdline, *prompt, **arglist;
+  int   result;
+  void  setup();
 
   prompt = DFL_PROMPT ;
   setup();
@@ -26,45 +26,51 @@ int main()
   while ( (cmdline = next_cmd(prompt, stdin)) != NULL )
     {
       if ((arglist = splitline(cmdline)) != NULL)
-	{
-	  // Edit his code: compared command from user to see if he wan to get out. 
-	  if(strcmp(arglist[0], "exit") == 0) 
-	    {
-	      if(arglist[1] == NULL) 
-		{
-		  exit(0);
-		}
-	      else 
-		{
-		  nRetCode = atoi(arglist[1]);
-		  exit(nRetCode);		
-		} 		
-	    }//end of if
-	  
-	  //problem code
-	  if((strcmp(arglist[0], "cd") == 0)  )
-	    {
-	      break;
-	    }
-	  else if( strncmp("cd", cmdline, 2) == 0 )
-	    {
-	      char *token = strtok(cmdline, " ");
-	      token = strtok(NULL, " ");
-	      
-	      chdir(token);
-	    }
-	  else
-	    {
-	      system(cmdline);
-	    }
-	  //end of problem code
+        {
 
-	  result = execute(arglist);
-	  freelist(arglist);	  
-	  free(cmdline);
-	}//end of if
-    }//end of while
-    return 0;
+          // Edit his code: compared command from user to see if he wan to get out.
+          if(strcmp(arglist[0], "exit") == 0)
+            {
+              if(arglist[1] == NULL)
+                {
+                  exit(0);
+                }
+              else
+                {
+                  nRetCode = atoi(arglist[1]);
+                  exit(nRetCode);
+                }
+            }//end of if
+
+          //problem code
+          if((strcmp(arglist[0], "cd") == 0)  )
+            {
+              break;
+            }
+          else if( strncmp("cd", cmdline, 2) == 0 )
+            {
+              char *token = strtok(cmdline, " ");
+              token = strtok(NULL, " ");
+
+              chdir(token);
+            }
+          else if (arglist[1] != NULL &&  strncmp(arglist[1], "..", 2))
+            {
+              system("cd ..");
+            }
+          else
+            {
+              system(cmdline);
+              return 0;
+            }
+          //end of problem code
+
+          result = execute(arglist);
+          freelist(arglist);
+          free(cmdline);
+        }//end of if
+        return 0;
+    }
 }//end of main
 
 void setup()
@@ -82,5 +88,3 @@ void fatal(char *s1, char *s2, int n)
   fprintf(stderr,"Error: %s,%s\n", s1, s2);
   exit(n);
 }
-
-
